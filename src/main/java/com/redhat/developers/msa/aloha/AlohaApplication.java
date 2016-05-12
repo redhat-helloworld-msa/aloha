e1ea6231b73faa0cd26f092d9ab7185e79be2634
eformat <eformat@gmail.com>
2016-05-12 16:23:04 +1200

    foo.com

--- src/main/java/com/redhat/developers/msa/aloha/AlohaApplication.java
+++ src/main/java/com/redhat/developers/msa/aloha/AlohaApplication.java
@@ -21,6 +21,9 @@ import io.vertx.core.Vertx;
 public class AlohaApplication {
 
 	public static void main(String[] args) {
+        System.setProperty("vertx.cacheDirBase", "/tmp");
+    	System.out.println("vertx.cacheDirBase:" + System.getProperty("vertx.cacheDirBase"));
+		System.setProperty("vertx.cwd", "/tmp");
 		Vertx vertx = Vertx.vertx();
 		vertx.deployVerticle(new AlohaVerticle());
 	}
